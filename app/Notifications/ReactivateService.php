0bfdd07cf34b519cea205d02562558d799ed1d0b
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2020-10-11 22:15:57 -0300

    Implementacion de supervisor en el servidor para el trabajo con colas

--- app/Notifications/ReactivateService.php
+++ app/Notifications/ReactivateService.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ReactivateService extends Notification
+class ReactivateService extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/UpdatePricesReminder.php
+++ app/Notifications/UpdatePricesReminder.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UpdatePricesReminder extends Notification
+class UpdatePricesReminder extends Notification implements ShouldQueue
 {
     use Queueable;
 
